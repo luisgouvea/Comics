0a3bea9e97b6799dbd81a20c344379ce29c78653
luisgouvea <luiseduardo.gouvea@hotmail.com>
2019-10-07 23:26:47 -0300

    nckanc dnask

---
+++ app/src/main/java/com/exame/luiseduardo/comics/activity/ListCharacterActivity.java
@@ -0,0 +1,15 @@
+package com.exame.luiseduardo.comics.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.exame.luiseduardo.comics.R;
+
+public class ListCharacterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_character);
+    }
+}
--- app/src/main/java/com/exame/luiseduardo/comics/activity/LoginActivity.java
+++ app/src/main/java/com/exame/luiseduardo/comics/activity/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.exame.luiseduardo.comics;
+package com.exame.luiseduardo.comics.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.exame.luiseduardo.comics.R;
 
 public class LoginActivity extends AppCompatActivity {
 
